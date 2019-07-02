9887ba836edcd59ea359b1942b8dc0983e211386
Mourya Baruah <mouryabaruah@gmail.com>
2019-07-02 09:49:35 +0000

    fix derp

--- parts/src/org/lineage/settings/BootCompletedReceiver.java
+++ parts/src/org/lineage/settings/BootCompletedReceiver.java
@@ -22,7 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineage.settings.dirac.DiracUtils;
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineage.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
