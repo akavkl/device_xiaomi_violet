cb2d2f29af903905679231eca110b5ddf4a72193
Mourya Baruah <mouryabaruah@gmail.com>
2019-07-06 05:29:27 +0000

    lineageos --> lineage

--- parts/src/org/lineage/settings/BootCompletedReceiver.java
+++ parts/src/org/lineage/settings/BootCompletedReceiver.java
@@ -1,19 +1,10 @@
-/*
- * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2018 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* * Copyright (C) 2015 The CyanogenMod Project * 2017-2018 The LineageOS Project * * Licensed under the 
+ Apache License, Version 2.0 (the "License"); * you may not use this file except in compliance with the 
+ License. * You may obtain a copy of the License at * * http://www.apache.org/licenses/LICENSE-2.0 * * 
+ Unless required by applicable law or agreed to in writing, software * distributed under the License is 
+ distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ implied. * See the License for the specific language governing permissions and * limitations under the 
+ License. */
 
 package org.lineage.settings;
 
@@ -22,7 +13,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.dirac.DiracUtils;
+import org.lineage.settings.dirac.DiracUtils;
 import org.lineage.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
