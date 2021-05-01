7a2ff462f8f0e67ad68d7012faca0427cb6a742e
trpsl <trpsl755@gmail.com>
2021-05-01 12:25:31 +0000

    Build pixel thermal hal

--- device.mk
+++ device.mk
@@ -414,10 +414,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
-
+    android.hardware.thermal@2.0-service.violet
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
